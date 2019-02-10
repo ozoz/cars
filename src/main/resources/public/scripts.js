ee73115baef8d3c1f5223b9b977b6daca46e17d7
keffa98 <a.fonoton@epmistes.net>
2019-02-10 15:35:14 +0100

    Add Car controller

--- src/main/resources/public/scripts.js
+++ src/main/resources/public/scripts.js
@@ -15,3 +15,20 @@ request.onload = function () {
 
 // Send request
 request.send();
+
+request = new XMLHttpRequest();
+request.open('POST', 'http://localhost:50001/api/v1/car', true);
+request.setRequestHeader("Content-type", "application/json");
+
+var parameters = {
+    "name": "toyota"
+};
+
+request.onload = function () {
+// Begin accessing JSON data here
+    var data = JSON.parse(this.response);
+    console.log(data);
+    document.getElementById("root").innerHTML = this.response;
+};
+
+request.send(JSON.stringify(parameters));
